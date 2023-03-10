314089c125f480ec153bbc15eac28ca3ceef920e
Harpal Singh1 <harpalnitk1@gmail.com>
2023-03-10 11:41:04 +0530

    start

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import {} from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
- <h1>Hello World!</h1>
-    </div>
-  )
-}
+const HomePage = (props) => {
+     return (
+         <div>
+            <h1>The Home Page</h1>
+         </div>
+     );
+        }
+
+
+export default HomePage;
